5371a97958d49bb0a235cca4a779b0fd34dd9203
Oli Somtochukwu <soli@seamfix.com>
2018-08-31 19:04:00 +0100

    Added new sim reg view

---
+++ src/test/java/com/sf/testCases/captureNewSimSerialRegistration.java
@@ -0,0 +1,43 @@
+package com.sf.testCases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+import com.sf.utils.testBase;
+
+public class captureNewSimSerialRegistration extends testBase{
+	
+	@Test
+    public void navigateToCaptureMenuTest() throws InterruptedException {
+	Thread.sleep(500);
+    WebDriverWait wait = new WebDriverWait(getDriver(), 30);
+    wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//android.widget.TextView[@text='Home']")));
+    Thread.sleep(500);
+    getDriver().findElement(By.className("android.widget.ImageButton")).click();
+    Thread.sleep(500);
+    getDriver().findElement(By.xpath("//android.widget.CheckedTextView[@text='Capture Subscriber']")).click();
+    wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//android.widget.TextView[@text='[Select Registration Type]']")));
+    getDriver().findElement(By.id("android:id/text1")).click();
+    Thread.sleep(500);
+    getDriver().findElement(By.xpath("//android.widget.CheckedTextView[@text='New Registration (Sim Serial)']")).click();
+    Thread.sleep(500);
+    getDriver().findElement(By.id("com.sf.biocapture.activity:id/next_button")).click();
+    wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("com.sf.biocapture.activity:id/add_sim_serial")));
+    
+    //Enter SIM Serial
+    getDriver().findElement(By.id("com.sf.biocapture.activity:id/sim_serial_field")).clear();
+    getDriver().findElement(By.id("com.sf.biocapture.activity:id/sim_serial_field")).sendKeys("11111111111111111111");
+    
+    Thread.sleep(500);
+    getDriver().findElement(By.id("com.sf.biocapture.activity:id/add_sim_serial")).click();
+    wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("android:id/button1")));
+    getDriver().findElement(By.id("android:id/button1")).click(); // ok button
+    Thread.sleep(1000);
+    getDriver().findElement(By.id("com.sf.biocapture.activity:id/next_button")).click(); // ok button
+    Thread.sleep(1000);
+    wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//android.widget.TextView[@text='Personal Details']")));
+    
+	}
+}
--- src/test/java/com/sf/testCases/login.java
+++ src/test/java/com/sf/testCases/login.java
@@ -1,6 +1,8 @@
 package com.sf.testCases;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 import com.sf.utils.testBase;
@@ -9,7 +11,9 @@ public class login extends testBase{
 	
 	@Test
     public void usernamePasswordTest() throws InterruptedException {
-        Thread.sleep(9000);
+        Thread.sleep(500);
+        WebDriverWait wait = new WebDriverWait(getDriver(), 30);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//android.widget.TextView[@text='Login']")));
         getDriver().findElement(By.id("com.sf.biocapture.activity:id/otp_login")).click();
         Thread.sleep(1000);
         getDriver().findElement(By.id("com.sf.biocapture.activity:id/login_username")).clear();
@@ -17,7 +21,8 @@ public class login extends testBase{
         getDriver().findElement(By.id("com.sf.biocapture.activity:id/login_password")).clear();
         getDriver().findElement(By.id("com.sf.biocapture.activity:id/login_password")).sendKeys("bankole1!!");
         getDriver().findElement(By.id("com.sf.biocapture.activity:id/submit")).click();
-        Thread.sleep(3000);
+        Thread.sleep(1000);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//android.widget.TextView[@text='Home']")));
     }
 
 }
